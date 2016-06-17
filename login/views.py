0b4d89f08a75cd39ae0f59ef9868cae0b360ffda
shifeixiang <294175702@qq.com>
2016-06-17 16:10:43 +0800

    add ajax

--- login/views.py
+++ login/views.py
@@ -1,17 +1,16 @@
 #coding=utf-8
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views.decorators.csrf import csrf_exempt    #用于处理post请求出现的错误
 from django.shortcuts import render_to_response
+import json
+import simplejson
 from django.http import HttpResponse
 from login.models import User    #导入模型
 from django.contrib.auth.hashers import make_password, check_password  #导入加密解密库
 # Create your views here.
-def login(request):
-     return render_to_response('login.html')
-     # return render_to_response('test_motaikuang.html')
-
 @csrf_exempt   #处理Post请求出错的情况
-def login_auth(request):
+def login(request):
      error1 = False
      if 'username' in request.POST:
           name = request.POST['username']
@@ -20,7 +19,7 @@ def login_auth(request):
                error1 = True            #用户名或密码输入不完整错误
           else:
                try:
-                    login = User.objects.get(login_name=name)
+                    login = User.objects.get(login_name=name)       #通过get获取对象--在这里用户名唯一
                except:
                     error3 = True
                     return render_to_response('login.html',{"error3":error3})
@@ -32,3 +31,32 @@ def login_auth(request):
                     error2 = True
                     return render_to_response('login.html',{"error2":error2})      #密码胡账户名错误
      return render_to_response('login.html',{"error1":error1})
+
+@csrf_exempt   #处理Post请求出错的情况
+def login_ajax(request):
+     error1 = False
+     if 'username' in request.POST:
+          name = request.POST['username']
+          if User.objects.filter(login_name=name):      #通过get获取对象--在这里用户名唯一:
+               login = User.objects.get(login_name=name)
+               password = request.POST['password']
+               sha_pwd = login.pwd           #获取数据库的密文
+               if check_password(password,sha_pwd):           #登陆成功
+                    request.session['login_name'] = name
+                    data = {'message':'sucess',
+                            'flag':1,
+                            'redirect':'main.html'}
+               else:
+                    data = {'message':'密码错误！',
+                            'flag':0,
+                            'redirect':'login_error.html'}
+               return HttpResponse(json.dumps(data),content_type="application/json")
+          else:
+               data = {'message':'用户名不存在！',
+                       'flag':0,
+                       'redirect':'login_error.html'}
+               return HttpResponse(json.dumps(data),content_type="application/json")
+     return render(request,'login_ajax.html')
+
+def main(request):
+     return render_to_response('main.html')
--- mulog/urls.py
+++ mulog/urls.py
@@ -1,11 +1,13 @@
+#coding=utf-8
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-from login.views import login,login_auth
+from login.views import login,login_ajax,main
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/',login),
-    url(r'^login_auth/',login_auth)
+    url(r'^login_ajax/$',login_ajax),   #不加上$符号，所有以login_ajax开头的都能匹配到
+    url(r'^login_ajax/main.html/$',main),
 )
